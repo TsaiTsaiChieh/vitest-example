d8d2045675263a931e073585bbee3a340142feb4
Tsai-Chieh <d0177381@gmail.com>
2023-02-03 16:17:44 +0800

    :heavy_plus_sign: should add jsdom, or will show ReferenceError: document is not defined error

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
+    environment: "jsdom",
     coverage: {
       reporter: ['text', 'json', 'html'],
     },
