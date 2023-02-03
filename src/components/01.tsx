9d5a06e35127c29f93c0c7b78acd86d1341a2976
Tsai-Chieh <d0177381@gmail.com>
2023-02-03 15:09:22 +0800

    :sparkles: Button01 done

--- src/components/01.tsx
+++ src/components/01.tsx
@@ -1,7 +1,7 @@
-const Button01 = () => {
-  return (
-    <div>01</div>
-  )
-}
+const Button01 = () => (
+  <button className='bg-green-500 hover:bg-green-600 text-white p-5 text-[30px] rounded-lg font-bold'>
+    Hi
+  </button>
+)
 
 export default Button01
