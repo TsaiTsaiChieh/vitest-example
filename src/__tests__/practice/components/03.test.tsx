46488f3a5eab36fa1c78ce016fb3fae4bf3f5cea
Tsai-Chieh <d0177381@gmail.com>
2023-02-06 16:19:07 +0800

    :tada: init 03 test case for practice

---
+++ src/__tests__/practice/components/03.test.tsx
@@ -0,0 +1,11 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+
+import Button03 from '../../../components/03'
+
+describe('mock function', () => {
+  test('fireEvent', () => {
+    // Given
+    // When
+    // Then
+  })
+})
