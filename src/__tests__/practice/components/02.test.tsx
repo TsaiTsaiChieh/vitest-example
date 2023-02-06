bf594f8287ae937d4bc71a3614ffc5e1baceb6ad
Tsai-Chieh <d0177381@gmail.com>
2023-02-06 15:48:52 +0800

    :tada: init 02 test for practice

---
+++ src/__tests__/practice/components/02.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+
+import Button02 from '../../../components/02'
+
+test('測試步驟、props', () => {
+  // Give, Arrange
+  // When, Act
+  // Then, Assert
+})
