4411b008c24ee83e1e33e346e1d7f61303f21bba
Tsai-Chieh <d0177381@gmail.com>
2023-02-06 16:44:49 +0800

    :sparkles: add userEvent

--- src/__tests__/answer/components/03.test.tsx
+++ src/__tests__/answer/components/03.test.tsx
@@ -1,8 +1,9 @@
 import { fireEvent, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import Button03 from '../../../components/03'
 
-describe('mock function', () => {
+describe('mock functions', () => {
   test('fireEvent', () => {
     // Given
     const content = 'Click'
@@ -10,6 +11,18 @@ describe('mock function', () => {
     // When
     render(<Button03 content={content} onClick={fn}/>)
     fireEvent.mouseDown(screen.getByRole('button', {
+      name: /^click$/i,
+    }))
+    // Then
+    expect(fn).toHaveBeenCalledTimes(1)
+  })
+  test('userEvent', async () => {
+    // Given
+    const content = 'Click'
+    const fn = vi.fn()
+    // When
+    render(<Button03 content={content} onClick={fn}/>)
+    await userEvent.click(screen.getByRole('button', {
       name: /^click$/i,
     }))
     // Then
--- src/__tests__/practice/components/03.test.tsx
+++ src/__tests__/practice/components/03.test.tsx
@@ -1,9 +1,15 @@
 import { fireEvent, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import Button03 from '../../../components/03'
 
 describe('mock function', () => {
   test('fireEvent', () => {
+    // Given
+    // When
+    // Then
+  })
+  test('userEvent', async () => {
     // Given
     // When
     // Then
