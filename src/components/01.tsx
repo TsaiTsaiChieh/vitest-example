606dc40e5ff3f4e7fdbc9a31c507123a925d3adb
Tsai-Chieh <d0177381@gmail.com>
2023-02-06 13:16:13 +0800

    :lipstick: rm hover

--- src/components/01.tsx
+++ src/components/01.tsx
@@ -1,5 +1,5 @@
 const Button01 = () => (
-  <button className='bg-green-500 hover:bg-green-600 text-white p-5 text-[30px] rounded-lg font-bold'>
+  <button className='bg-green-500 text-white p-5 text-[30px] rounded-lg font-bold'>
     Hi
   </button>
 )
