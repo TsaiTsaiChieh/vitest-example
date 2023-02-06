71e6104509ab6aa180769e8ede91dd9cbe86161b
Tsai-Chieh <d0177381@gmail.com>
2023-02-06 15:54:14 +0800

    :sparkles: finish Button03 component

--- src/components/03.tsx
+++ src/components/03.tsx
@@ -1,6 +1,15 @@
-const Button03 = () => {
+interface Props {
+  content: string
+  onClick: () => void
+}
+const Button03 = ({ content, onClick }: Props) => {
   return (
-    <div>03</div>
+    <button
+      className='bg-yellow-500 text-white p-5 text-[30px] rounded-lg font-bold'
+      onMouseDown={onClick}
+    >
+      {content}
+    </button>
   )
 }
 
