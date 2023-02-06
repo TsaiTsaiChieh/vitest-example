d61214f2c6ef5e78ab34dc928d01bf78e9a826b8
Tsai-Chieh <d0177381@gmail.com>
2023-02-06 13:15:33 +0800

    :wrench: tune test setting, ref from https://pjchender.dev/npm/note-vite-vitest/

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
+    globals: true,
     environment: "jsdom",
+    includeSource: ["src/__tests__/*.{js,tsx,ts}"],
+    setupFiles: 'src/setupTests.ts',
     coverage: {
-      reporter: ['text', 'json', 'html'],
+      reporter: ["text", "json", "html"],
     },
   },
   plugins: [react()],
-})
+});
