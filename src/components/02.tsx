4a7fb3f13c26fbf0aa9a2187adbdb0624aed6572
Tsai-Chieh <d0177381@gmail.com>
2023-02-06 15:48:07 +0800

    :sparkles: finish button02 component

--- src/components/02.tsx
+++ src/components/02.tsx
@@ -1,7 +1,10 @@
-const Button02 = () => {
-  return (
-    <div>02</div>
-  )
+interface Props {
+  content: string
 }
+const Button02 = ({ content }: Props) => (
+  <button className='bg-green-500 text-white p-5 text-[30px] rounded-lg font-bold'>
+    {content}
+  </button>
+)
 
 export default Button02
