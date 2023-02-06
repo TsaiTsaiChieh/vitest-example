7dd28d12286a5827c3430c6cb0ff02597aa38374
Tsai-Chieh <d0177381@gmail.com>
2023-02-06 14:53:10 +0800

    :tada: init first test for practice

---
+++ src/__tests__/practice/components/01.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+
+import Button01 from '../../../components/01'
+
+test('第一個測試', () => {
+  // 1. render 出要測試的元件
+  // 2. 在畫面上抓到該元件
+  // 4. 檢查結果是否和期待相符
+
+})
+
+test('快照', () => {
+  // 1. 2. render 出要測試的元件 && 在畫面上抓到該元件
+  // 4. 檢查結果是否和期待相符
+})
