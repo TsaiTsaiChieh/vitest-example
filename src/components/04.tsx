783a747fb94243b0f2bae38aa392a8098beea442
Tsai-Chieh <d0177381@gmail.com>
2023-02-07 11:46:54 +0800

    :sparkles: finish button04 component

--- src/components/04.tsx
+++ src/components/04.tsx
@@ -1,6 +1,24 @@
+import { useState } from 'react'
+
 const Button04 = () => {
+  const [loading, setLoading] = useState<boolean>(false)
+  const handleOnClick = () => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }
   return (
-    <div>04</div>
+    <div className='flex flex-col'>
+      <button
+        disabled={loading}
+        className='bg-red-500 text-white p-5 text-[30px] rounded-lg font-bold disabled:opacity-80'
+        onClick={handleOnClick}
+      >
+        Click Me
+      </button>
+      {loading && <span className='font-base m-1'>Loading...</span>}
+    </div>
   )
 }
 
