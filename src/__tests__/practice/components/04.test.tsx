1de5b51dcea3f04b977c5dae221809a326df1b91
Tsai-Chieh <d0177381@gmail.com>
2023-02-07 14:58:54 +0800

    :tada: init Button04 for practice

---
+++ src/__tests__/practice/components/04.test.tsx
@@ -0,0 +1,17 @@
+import { act, fireEvent, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import Button04 from '../../../components/04'
+
+describe('mock 時間、act', () => {
+  test('fireEvent', () => {
+    // Given
+    // When
+    // Then
+  })
+  test('userEvent', async () => {
+    // Given
+    // When
+    // Then
+  })
+})
