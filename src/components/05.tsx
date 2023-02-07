417bcbe32c878d7595760b172bed8937ba96b748
Tsai-Chieh <d0177381@gmail.com>
2023-02-07 16:50:51 +0800

    :sparkles: finish Button05 component

--- src/components/05.tsx
+++ src/components/05.tsx
@@ -1,7 +1,15 @@
+import styled from 'styled-components'
+
 const Button05 = () => {
-  return (
-    <div>05</div>
-  )
+  const StyledButton = styled.button`
+    background: #77bb1b;
+    color: white;
+    padding: 1.25rem;
+    font-weight: 700;
+    font-size: 30px;
+    border-radius: 8px;
+  `
+  return <StyledButton>Button</StyledButton>
 }
 
 export default Button05
