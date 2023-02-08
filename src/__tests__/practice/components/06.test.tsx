c4c164ddd6d9061e7fe486970ed653a2eb6192a1
Tsai-Chieh <d0177381@gmail.com>
2023-02-08 14:26:53 +0800

    :tada: init Button06 for practice

---
+++ src/__tests__/practice/components/06.test.tsx
@@ -0,0 +1,45 @@
+import { render } from '@testing-library/react'
+
+import Button06 from '../../../components/06'
+import { ButtonEnum } from '../../../constants/enum'
+
+describe('(First) Button with different types', () => {
+  // Give
+
+  test('Red type', () => {
+    // When
+    // Then
+  })
+  test('Green type', () => {
+    // When
+    // Then
+  })
+  test('Blue type', () => {
+    // When
+    // Then
+  })
+  test('X type', () => {
+    // When
+    // Then
+  })
+})
+
+describe('(Rerender) Button with different types', () => {
+  test('Test all types', () => {
+  })
+})
+
+describe('(Each: Array) Button with different types', () => {
+  test.each([[]])('Button with type: %s', () => {
+  })
+})
+
+describe('(Each: Object) Button with different types', () => {
+  test.each([{}])('Button with type: $type', () => {
+  })
+})
+
+describe('(Each: Table) Button with different types', () => {
+  test.each``('Button with type: $type', () => {
+  })
+})
