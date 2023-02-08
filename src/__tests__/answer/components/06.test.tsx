e48c4d6dde0a8355fcbe3d8f165bd39f8b64e8c2
Tsai-Chieh <d0177381@gmail.com>
2023-02-08 11:47:24 +0800

    :sparkles: finish Button with different types for first time

---
+++ src/__tests__/answer/components/06.test.tsx
@@ -0,0 +1,42 @@
+import { render } from '@testing-library/react'
+
+import Button06 from '../../../components/06'
+import { ButtonEnum } from '../../../constants/enum'
+
+describe('(First) Button with different types', () => {
+  // Give
+  const Clx = {
+    [ButtonEnum.RED]: 'bg-red-500',
+    [ButtonEnum.GREEN]: 'bg-green-500',
+    [ButtonEnum.BLUE]: 'bg-blue-500',
+    X: 'bg-blue-500',
+  }
+  test('Red type', () => {
+    // When
+    const { getByRole } = render(<Button06 type={ButtonEnum.RED}/>)
+    const button = getByRole('button', { name: /^Button$/i })
+    // Then
+    expect(button).toHaveClass(Clx[ButtonEnum.RED])
+  })
+  test('Green type', () => {
+    // When
+    const { getByRole } = render(<Button06 type={ButtonEnum.GREEN}/>)
+    const button = getByRole('button', { name: /^Button$/i })
+    // Then
+    expect(button).toHaveClass(Clx[ButtonEnum.GREEN])
+  })
+  test('Blue type', () => {
+    // When
+    const { getByRole } = render(<Button06 type={ButtonEnum.BLUE}/>)
+    const button = getByRole('button', { name: /^Button$/i })
+    // Then
+    expect(button).toHaveClass(Clx[ButtonEnum.BLUE])
+  })
+  test('X type', () => {
+    // When
+    const { getByRole } = render(<Button06 type={ButtonEnum.BLUE}/>)
+    const button = getByRole('button', { name: /^Button$/i })
+    // Then
+    expect(button).toHaveClass(Clx.X)
+  })
+})
