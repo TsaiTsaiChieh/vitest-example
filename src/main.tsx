231cfc7ea5c435ed627790be27ec9b4587deae4b
Tsai-Chieh <d0177381@gmail.com>
2023-02-08 16:24:37 +0800

    :sparkles: start worker, ref from https://mswjs.io/docs/getting-started/integrate/browser

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,10 @@ import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = await import('./mocks/browser')
+  await worker.start()
+}
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <React.StrictMode>
