94dad4e208e12114d5ee3e7975739b4572ddb17a
Tsai-Chieh <d0177381@gmail.com>
2023-02-08 10:55:57 +0800

    :sparkles: change type to optional prop

--- src/components/06.tsx
+++ src/components/06.tsx
@@ -1,7 +1,28 @@
-const Button06 = () => {
-  return (
-    <div>06</div>
-  )
+import { ButtonEnum } from '../constants/enum'
+
+interface Props {
+  type?: string | ButtonType
+}
+const Button06 = ({ type }: Props) => (
+  <button
+    className={`${getClx(
+      type,
+    )} text-white p-5 text-[30px] rounded-lg font-bold`}
+  >
+    Button
+  </button>
+)
+
+const getClx = (type?: string | ButtonType): string => {
+  const defaultClx = 'bg-blue-500'
+  switch (type) {
+    case ButtonEnum.RED:
+      return 'bg-red-500'
+    case ButtonEnum.GREEN:
+      return 'bg-green-500'
+    default:
+      return defaultClx
+  }
 }
 
 export default Button06
