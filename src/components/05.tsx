ea529cf325d60b9aeec51087a690f8c3317340e9
Tsai-Chieh <d0177381@gmail.com>
2023-02-08 10:23:45 +0800

    :bug: style should outside the component

--- src/components/05.tsx
+++ src/components/05.tsx
@@ -1,14 +1,14 @@
 import styled from 'styled-components'
 
+const StyledButton = styled.button`
+  background: #77bb1b;
+  color: white;
+  padding: 1.25rem;
+  font-weight: 700;
+  font-size: 30px;
+  border-radius: 8px;
+`
 const Button05 = () => {
-  const StyledButton = styled.button`
-    background: #77bb1b;
-    color: white;
-    padding: 1.25rem;
-    font-weight: 700;
-    font-size: 30px;
-    border-radius: 8px;
-  `
   return <StyledButton>Button</StyledButton>
 }
 
