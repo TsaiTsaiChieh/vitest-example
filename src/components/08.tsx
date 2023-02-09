6249c30edb088eadf5e0d32851fb97b35f20f65f
Tsai-Chieh <d0177381@gmail.com>
2023-02-09 15:03:24 +0800

    :sparkles: finish button08 component

--- src/components/08.tsx
+++ src/components/08.tsx
@@ -1,6 +1,16 @@
+import { useCounter } from '../hooks/useCounter'
 const Button08 = () => {
+  const { count, increment } = useCounter()
+
   return (
-    <div>08</div>
+    <div className="flex flex-col">
+      <span className='font-bold text-2xl mb-2'>{count}</span>
+    <button className='bg-black text-white p-5 text-[30px] rounded-lg font-bold'
+    onClick={increment}
+    >
+    Increment
+  </button>
+  </div>
   )
 }
 
