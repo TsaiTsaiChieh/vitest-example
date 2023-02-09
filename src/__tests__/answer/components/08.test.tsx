e60fe612640f7c1b1dfbe45848a425a2246ec602
Tsai-Chieh <d0177381@gmail.com>
2023-02-09 15:41:22 +0800

    :sparkles: finish renderHook test example

--- src/__tests__/answer/components/08.test.tsx
+++ src/__tests__/answer/components/08.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, renderHook } from '@testing-library/react'
 import { act } from 'react-dom/test-utils'
 
 import { useCounter } from '../../../hooks/useCounter'
@@ -11,11 +11,19 @@ describe('react hook', () => {
       return null
     }
     render(<TestComponent />)
-    screen.debug()
     expect(result.count).toBe(0)
     act(() => {
       result.increment()
     })
     expect(result.count).toBe(1)
+  })
+
+  test('renderHook', async () => {
+    const { result } = renderHook(() => useCounter())
+    expect(result.current.count).toBe(0)
+    act(() => {
+      result.current.increment()
+    })
+    expect(result.current.count).toBe(1)
   })
 })
