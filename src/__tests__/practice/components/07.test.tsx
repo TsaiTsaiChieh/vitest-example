b3f4a80fb3dc21b0560c7e0ad028d0c2621234a6
Tsai-Chieh <d0177381@gmail.com>
2023-02-09 15:05:35 +0800

    :tada: init button07 for practice

---
+++ src/__tests__/practice/components/07.test.tsx
@@ -0,0 +1,30 @@
+import {
+  render,
+  screen,
+  waitFor,
+  within,
+} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { rest } from 'msw'
+
+import Button07 from '../../../components/07'
+import { server } from '../../../mocks/server'
+
+describe('Test /week API', () => {
+  test('API loading', async () => {
+    // Given
+    // When
+    // Then
+
+  })
+  test('API success', async () => {
+    // Given
+    // When
+    // Then
+  })
+  test('API fail', async () => {
+    // Given
+    // When
+    // Then
+  })
+})
