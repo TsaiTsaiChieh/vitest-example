5900835010321fd309385a8df871bbadba96a477
Tsai-Chieh <d0177381@gmail.com>
2023-02-15 13:29:24 +0800

    :boom: add RTK-Q with MSW, ref from https://gustavocd.dev/posts/testing-rtk-query-with-msw/

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,22 @@
 import '@testing-library/jest-dom'
+
+import { fetch, Headers, Request, Response } from 'cross-fetch'
+import { setupServer } from 'msw/node'
+
+import { handlers } from './mocks/handlers'
+
+// avoid CORS error in RTK-Query
+global.fetch = fetch
+global.Headers = Headers
+global.Request = Request
+global.Response = Response
+
+export const server = setupServer(...handlers)
+// Enable API mocking before tests.
+beforeAll(() => { server.listen() })
+
+// Reset any runtime request handlers we may add during the tests.
+afterEach(() => { server.resetHandlers() })
+
+// Disable API mocking after the tests are done.
+afterAll(() => { server.close() })
