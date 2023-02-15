1e1d751aeb767fcf43a7ee2beeee52cabd47dd54
Tsai-Chieh <d0177381@gmail.com>
2023-02-15 09:34:59 +0800

    :tada: init Button 07 for RTK

---
+++ src/components/07-1.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+import { useLazyGetWeekQuery } from '../services/api'
+
+const Button07_ = () => {
+  const [getWeek, { isLoading, error }] = useLazyGetWeekQuery()
+
+  const [data, setData] = useState<string[]>([])
+
+  const handleClick = async () => {
+    await getWeek()
+      .unwrap()
+      .then((result) => {
+        setData(result.days)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  return (
+    <div className='flex flex-col'>
+      <button
+        className='bg-amber-700 text-white p-5 text-[30px] rounded-lg font-bold'
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        onClick={handleClick}
+      >
+        Get Data
+      </button>
+      <div className='mt-2'>
+        {isLoading
+          ? (
+          <span>Loading...</span>
+            )
+          : (
+          <ul>
+            {data.length !== 0 &&
+              data.map((ele: string) => <li key={ele}>{ele}</li>)}
+          </ul>
+            )}
+        {error && <span>{JSON.stringify(error)}</span>}
+      </div>
+    </div>
+  )
+}
+
+export default Button07_
