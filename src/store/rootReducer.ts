b90b1a9e36fbee9b98c81c3d02d192be0259108e
Tsai-Chieh <d0177381@gmail.com>
2023-02-15 09:24:11 +0800

    :sparkles: add counter slice

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit'
 
 import { api } from '../services/api'
+import counterReducer from './reducers/counterSlice'
 
 export const rootReducer = combineReducers({
   [api.reducerPath]: api.reducer,
+  counter: counterReducer,
 })
