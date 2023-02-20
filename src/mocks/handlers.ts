e9407986d792748daf030aeb304c8c1e9fd424c8
Tsai-Chieh <d0177381@gmail.com>
2023-02-20 16:53:01 +0800

    :sparkles: add delay commanded out

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -6,6 +6,7 @@ export const handlers = [
     `${VITE_APP_URL}/week`,
     (_, res, ctx) => {
       return res(
+        // ctx.delay(1000),
         ctx.status(200),
         ctx.json({
           days: ['Mon', 'Tue', 'Web', 'Thu', 'Fri', 'Sat', 'Sun'],
