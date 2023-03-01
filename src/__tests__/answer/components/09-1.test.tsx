7e4c997c0faf34fbf0151ec4700b68f4ba43c894
Tsai-Chieh <d0177381@gmail.com>
2023-03-01 14:58:43 +0800

    :bug: should check container

--- src/__tests__/answer/components/09-1.test.tsx
+++ src/__tests__/answer/components/09-1.test.tsx
@@ -9,7 +9,7 @@ describe('<Decorate />', () => {
     const num = 1000
     // When
     const { getByRole } = render(<Decorate num={num} />)
-    screen.debug()
+    // screen.debug()
     // Then
     expect(
       getByRole('heading', { name: `${num} 個木頭拿去裝飾` }),
@@ -20,16 +20,25 @@ describe('<Decorate />', () => {
     // Given
     const num = 1000
     // When
-    const { getByRole } = render(<Decorate num={num} />)
+    const { container } = render(<Decorate num={num} />)
     // Then
-    expect(getByRole('heading', { name: `${num} 個木頭拿去裝飾` }))
-      .toMatchInlineSnapshot(`
-      <h2
-        class="font-bold text-2xl mb-4"
-      >
-        1000
-         個木頭拿去裝飾
-      </h2>
+    // screen.debug(container)
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <div
+          class="p-4 border-red-400 border w-[50%]"
+        >
+          <h2
+            class="font-bold text-2xl mb-4"
+          >
+            1000
+             個木頭拿去裝飾
+          </h2>
+          <span>
+            拿去裝飾小花園的圍籬
+          </span>
+        </div>
+      </div>
     `)
   })
 })
