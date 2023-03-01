092ea26dcfa2f0193a8e0aeb8e2bcaaae74a8428
Tsai-Chieh <d0177381@gmail.com>
2023-03-01 15:40:46 +0800

    :art: format

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import linaria from '@linaria/vite';
+import linaria from "@linaria/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,15 +9,18 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     includeSource: ["src/__tests__/*.{js,tsx,ts}"],
-    setupFiles: 'src/setupTests.ts',
+    setupFiles: "src/setupTests.ts",
     coverage: {
       reporter: ["text", "json", "html"],
     },
   },
-  plugins: [react(), linaria({
-    include: ['**/*.{ts,tsx}'],
-    babelOptions: {
-      presets: ['@babel/preset-typescript', '@babel/preset-react'],
-    },
-  })],
+  plugins: [
+    react(),
+    linaria({
+      include: ["**/*.{ts,tsx}"],
+      babelOptions: {
+        presets: ["@babel/preset-typescript", "@babel/preset-react"],
+      },
+    }),
+  ],
 });
